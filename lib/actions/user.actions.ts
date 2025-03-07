94a995e011a5293cece97eb908eec9b4ccda3dc6
m0chi/ <aly0mal99@gmail.com>
2025-03-07 14:50:45 +0100

    add verifySecret

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -5,6 +5,7 @@ import { parseStringify } from '../utils';
 import { appwriteConfig } from '../appwrite/config';
 import { Query, ID } from 'node-appwrite';
 import { avatarPlaceholderUrl } from '@/constants';
+import { cookies } from 'next/headers';
 
 const getUserByEmail = async (email: string) => {
   const { databases } = await createAdminClient();
@@ -23,7 +24,7 @@ const handleError = (error: unknown, message: string) => {
   throw error;
 }
 
-const sendEmailOTP = async ({ email }: { email: string}) => {
+export const sendEmailOTP = async ({ email }: { email: string}) => {
   const { account } = await createAdminClient();
 
   try {
@@ -60,3 +61,22 @@ export const createAccount = async ({ fullName, email }: { fullName: string; ema
 
   return parseStringify({ accountId });
 }
+
+export const verifySecret = async ({ accountId, password }: { accountId: string; password: string; }) => {
+  try {
+    const { account } = await createAdminClient();
+
+    const session = await account.createSession(accountId, password);
+
+    (await cookies()).set('appwrite-session', session.secret, {
+      path: '/',
+      httpOnly: true,
+      sameSite: 'strict',
+      secure: true,
+    });
+
+    return parseStringify({ sessionId: session.$id });
+  } catch (error) {
+    handleError(error, 'Failed to verify OTP');
+  }
+};
