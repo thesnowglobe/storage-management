ed9a1586bc9bfa4db8cd3e1079a72fac1ec82cf1
m0chi/ <aly0mal99@gmail.com>
2025-03-12 12:47:32 +0100

    update constants

--- constants/index.ts
+++ constants/index.ts
@@ -55,4 +55,32 @@ export const sortTypes = [
   },
 ];
 
+export const actionsDropdownItems = [
+  {
+    label: "Rename",
+    icon: "/assets/icons/edit.svg",
+    value: "rename",
+  },
+  {
+    label: "Details",
+    icon: "/assets/icons/info.svg",
+    value: "details",
+  },
+  {
+    label: "Share",
+    icon: "/assets/icons/share.svg",
+    value: "share",
+  },
+  {
+    label: "Download",
+    icon: "/assets/icons/download.svg",
+    value: "download",
+  },
+  {
+    label: "Delete",
+    icon: "/assets/icons/delete.svg",
+    value: "delete",
+  },
+];
+
 export const MAX_FILE_SIZE = 50 * 1024 * 1024; // 50MB
